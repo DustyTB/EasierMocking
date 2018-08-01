775f086194a8cc2666de15259f6569442030fa73
DustyTB <36819219+DustyTB@users.noreply.github.com>
2018-08-01 02:12:07 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace EasierMocking
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openConverterButton = new System.Windows.Forms.Button();
+            this.toggleCb = new System.Windows.Forms.CheckBox();
+            this.cTextbox = new System.Windows.Forms.RichTextBox();
+            this.cButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openConverterButton
+            // 
+            this.openConverterButton.Location = new System.Drawing.Point(12, 12);
+            this.openConverterButton.Name = "openConverterButton";
+            this.openConverterButton.Size = new System.Drawing.Size(145, 23);
+            this.openConverterButton.TabIndex = 0;
+            this.openConverterButton.Text = "Converter";
+            this.openConverterButton.UseVisualStyleBackColor = true;
+            // 
+            // toggleCb
+            // 
+            this.toggleCb.AutoSize = true;
+            this.toggleCb.Location = new System.Drawing.Point(12, 41);
+            this.toggleCb.Name = "toggleCb";
+            this.toggleCb.Size = new System.Drawing.Size(59, 17);
+            this.toggleCb.TabIndex = 1;
+            this.toggleCb.Text = "Toggle";
+            this.toggleCb.UseVisualStyleBackColor = true;
+            // 
+            // cTextbox
+            // 
+            this.cTextbox.Location = new System.Drawing.Point(207, 2);
+            this.cTextbox.Name = "cTextbox";
+            this.cTextbox.Size = new System.Drawing.Size(312, 67);
+            this.cTextbox.TabIndex = 2;
+            this.cTextbox.Text = "";
+            // 
+            // cButton
+            // 
+            this.cButton.BackColor = System.Drawing.Color.Transparent;
+            this.cButton.FlatAppearance.BorderSize = 0;
+            this.cButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cButton.Location = new System.Drawing.Point(525, 2);
+            this.cButton.Name = "cButton";
+            this.cButton.Size = new System.Drawing.Size(54, 60);
+            this.cButton.TabIndex = 3;
+            this.cButton.Text = "Convert";
+            this.cButton.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(584, 67);
+            this.Controls.Add(this.cButton);
+            this.Controls.Add(this.cTextbox);
+            this.Controls.Add(this.toggleCb);
+            this.Controls.Add(this.openConverterButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "EasierMocking";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openConverterButton;
+        private System.Windows.Forms.CheckBox toggleCb;
+        private System.Windows.Forms.RichTextBox cTextbox;
+        private System.Windows.Forms.Button cButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace EasierMocking
+{
+    public partial class Form1 : Form
+    {
+        private static bool panelOpen = false;
+        private static bool toggled = false;
+        private IKeyboardMouseEvents globalHook;
+
+        public Form1()
+        {
+            InitializeComponent();
+            RegEvent();
+            this.Width = 184;
+            openConverterButton.Text = "Open Converter";
+        }
+
+        public void Convert(object sender, EventArgs e)
+        {
+            int l = cTextbox.TextLength;
+            if (l == 0)
+            {
+                cTextbox.Text = "u should type something";
+            }
+            else
+            {
+                Random rand = new Random();
+                int num = rand.Next(0, 2);
+                char c;
+                string preconversion = cTextbox.Text;
+                cTextbox.Clear();
+                foreach (char i in preconversion)
+                {
+                    if (num == 0)
+                    {
+                        num = 1;
+                        c = char.ToUpper(i);
+                        cTextbox.AppendText(c.ToString());
+                        continue;
+                    }
+                    if (num == 1)
+                    {
+                        num = 0;
+                        c = char.ToLower(i);
+                        cTextbox.AppendText(c.ToString());
+                        continue;
+                    }
+                }
+            }
+        }
+
+        public void ConverterPanelTrigger(object sender, EventArgs e)
+        {
+            if (!panelOpen)
+            {
+                Width = Width + 416;
+                panelOpen = true;
+                openConverterButton.Text = "Close Converter";
+            }
+            else
+            {
+                Width = Width - 416;
+                panelOpen = false;
+                openConverterButton.Text = "Open Converter";
+            }
+        }
+
+        public void OnKP(object sender, KeyEventArgs e)
+        {
+            if (toggled)
+            {
+                const int KEYEVENTF_EXTENDEDKEY = 0x1;
+                const int KEYEVENTF_KEYUP = 0x2;
+                keybd_event(0x14, 0x45, KEYEVENTF_EXTENDEDKEY, (UIntPtr)0);
+                keybd_event(0x14, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP,
+                (UIntPtr)0);
+            }
+        }
+
+        public void OnToggle(object sender, EventArgs e)
+        {
+            if (toggleCb.CheckState == CheckState.Checked)
+            {
+                toggled = true;
+            }
+            if (toggleCb.CheckState == CheckState.Unchecked)
+            {
+                toggled = false;
+            }
+        }
+
+        public void RegEvent()
+        {
+            openConverterButton.Click += new EventHandler(ConverterPanelTrigger);
+            cButton.Click += new EventHandler(Convert);
+            toggleCb.CheckedChanged += new EventHandler(OnToggle);
+            globalHook = Hook.GlobalEvents();
+            globalHook.KeyDown += new KeyEventHandler(OnKP);
+        }
+        [DllImport("user32.dll")]
+        private static extern void keybd_event(byte bVk, byte bScan, uint dwFlags,
+UIntPtr dwExtraInfo);
+    }
+}
